ac3351495e594004eb90db98f9063d689a7c3014
amiel-danao <amielrenaissance4@gmail.com>
2023-01-20 19:41:27 +0800

    updated database settings

--- reviewcenter/settings.py
+++ reviewcenter/settings.py
@@ -113,6 +113,9 @@ else:
             "PASSWORD": "notCommonPassword1234",
             "HOST": "ncjp.mysql.pythonanywhere-services.com",
             "PORT": "3306",
+            "OPTIONS": {
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
         }
     }
 
