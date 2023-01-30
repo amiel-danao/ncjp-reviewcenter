abd57b314f93751f582f772075cbc965b581e043
amiel-danao <amielrenaissance4@gmail.com>
2023-01-30 12:53:37 +0800

    aaa

--- reviewcenter/settings.py
+++ reviewcenter/settings.py
@@ -117,7 +117,7 @@ else:
             "HOST": "ncjp.mysql.pythonanywhere-services.com",
             "PORT": "3306",
             "OPTIONS": {
-                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                # "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4'
             },
         }
